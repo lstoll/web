a59225550f3997d40f5e6a43722729a482f9acc4
Lincoln Stoll <me@lds.li>
2025-06-20 13:29:10 +0200

    generate our own UUID

--- go.mod
+++ go.mod
@@ -5,8 +5,7 @@ go 1.24
 require (
 	filippo.io/csrf v0.0.0-20250517103426-cfb6fbb0fbe3
 	github.com/google/go-cmp v0.7.0
-	github.com/google/uuid v1.6.0
-	golang.org/x/crypto v0.37.0
+	golang.org/x/crypto v0.39.0
 )
 
-require golang.org/x/sys v0.32.0 // indirect
+require golang.org/x/sys v0.33.0 // indirect
--- requestid/context.go
+++ requestid/context.go
@@ -2,8 +2,8 @@ package requestid
 
 import (
 	"context"
-
-	"github.com/google/uuid"
+	"crypto/rand"
+	"fmt"
 )
 
 type requestIDCtxKey struct{}
@@ -27,6 +27,28 @@ func FromContext(ctx context.Context) (_ string, ok bool) {
 	return v, ok
 }
 
+// newRequestID generates a new uuidv4
 func newRequestID() string {
-	return uuid.Must(uuid.NewV7()).String()
+	var uuid [16]byte
+
+	// Fill the entire UUID with cryptographically secure random bytes.
+	if _, err := rand.Read(uuid[:]); err != nil {
+		panic(fmt.Sprintf("requestid: reading random bytes: %v", err))
+	}
+
+	// Set the version to 4.
+	// The 7th byte's most significant 4 bits are set to 0100.
+	// uuid[6] = (uuid[6] & 0x0F) | 0x40
+	// Clear the first 4 bits and then set the 4th bit.
+	uuid[6] = (uuid[6] & 0b00001111) | 0b01000000
+
+	// Set the variant to RFC 4122 (10xx).
+	// The 9th byte's most significant 2 bits are set to 10.
+	// uuid[8] = (uuid[8] & 0x3F) | 0x80
+	// Clear the first 2 bits and then set the 2nd bit.
+	uuid[8] = (uuid[8] & 0b00111111) | 0b10000000
+
+	// Format the UUID into the standard 8-4-4-4-12 string representation.
+	return fmt.Sprintf("%08x-%04x-%04x-%04x-%012x",
+		uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:16])
 }
